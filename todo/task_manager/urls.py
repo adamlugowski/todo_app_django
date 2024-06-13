823bde36bdf34810d3f90353c910ea117ead93c4
Adam Ługowski <adam.lugowski@live.com>
2024-06-13 22:01:18 +0200

    added index and form to add task

--- todo/task_manager/urls.py
+++ todo/task_manager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import tasks_list
+from .views import tasks_list, index, add_task
 
 urlpatterns = [
-    path('tasks', tasks_list)
+    path('', index, name='index'),
+    path('tasks', tasks_list, name='tasks_list'),
+    path('add_task', add_task, name='add_task')
 ]
